8dd540bd19a22bc5f8b5c2374aaa91d6dee87ead
David Fontenot <font21@gmail.com>
2020-05-08 12:49:50 -0500

    Going to lunch

--- js/app.js
+++ js/app.js
@@ -1,5 +1,37 @@
-// Message if JavaScript is connected and running
-    console.log('JavaScript connected.');
+// Message if scripts are connected and running
+	console.log('JavaScript connected.');
+	
+// jQuery Resursive Wrap
+$(() => {
+	console.log('jQuery connected.');
+	
+// Starting variables
+	let TheDisplayedNumber = 0;
+	let incrementVar = 0;
 
-    console.log('jQuery connected.');
+// Functions
+	incrementorator = (doToVar, whatDoVar) => {
+		doToVar = TheDisplayedNumber doToVar whatDoVar;
+		upperText = TheDisplayedNumber;
+		return
+	}
 
+	incrementationDisplay = () => {
+		
+	}
+// Setup Page
+	upperText = $('.upper');
+	$upperText.text('Incrementorator');
+
+
+// Input text Box
+
+// Buttons
+	on(press) = {
+		incrementorator( TheDisplayedNumber, incrementVar );
+	}
+
+
+
+// jQuery Resursive Wrap Close
+})
